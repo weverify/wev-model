64771bc30636ad7f533ee2f05a74233b3cc71a03
Andrey Tagarev <andrey.tagarev@ontotext.com>
2020-05-04 09:31:44 +0300

    WEV-395 Extended the WeVerify model with evidence types.

--- taxonomy/run.sh
+++ taxonomy/run.sh
@@ -4,5 +4,7 @@ curl "https://docs.google.com/spreadsheets/d/17Fmgow6BB0zkBXqk-sWEhoohOjo0bkpD2i
 
 curl "https://docs.google.com/spreadsheets/d/17Fmgow6BB0zkBXqk-sWEhoohOjo0bkpD2i4kqr-vjNg/gviz/tq?tqx=out:csv&sheet=reviewType" | tarql --stdin skos-concepts.tarql > rdf/tmp/reviewTypes.ttl
 
+curl "https://docs.google.com/spreadsheets/d/17Fmgow6BB0zkBXqk-sWEhoohOjo0bkpD2i4kqr-vjNg/gviz/tq?tqx=out:csv&sheet=evidenceType" | tarql --stdin skos-concepts.tarql > rdf/tmp/evidenceTypes.ttl
+
 riot  --formatted=ttl rdf/tmp/*.ttl > rdf/wev-taxonomy.ttl
 
